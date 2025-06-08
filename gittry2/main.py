4d248ffe449e98ea0946d217d23439b717f85e98
Vansh-sharma335 <vanshsharma@9821gmai.com>
2025-06-08 23:12:55 +0545

    i updated sub and divide

--- gittry2/main.py
+++ gittry2/main.py
@@ -3,3 +3,9 @@ input1 = input("Enter first number: ")
 input2 = input("Enter second number: ")
 result = int(input1) + int(input2)
 print("The sum is:", result)
+ 
+#code to subtract
+input1 = input("Enter first number: ")
+input2 = input("Enter second number: ") 
+result = int(input1) - int(input2)
+print("The difference is:", result)
---
+++ gittry2/main2.py
@@ -0,0 +1,5 @@
+#code to divide
+input1 = input("Enter first number: ")
+input2 = input("Enter second number: ")   
+result = int(input1) / int(input2)
+print("The division is:", result)
